e2c54ede06f92906fb94b922cdf3cdf8ed7233b0
Josh O'Brian <joshuaeobrian@gmail.com>
2018-04-19 20:27:59 -0400

    First commit

---
+++ src/entity/User.ts
@@ -0,0 +1,28 @@
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn} from "typeorm";
+import {Role} from "./Role";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({unique:true})
+    username:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    email:string;
+
+    @OneToOne(type => Role)
+    @JoinColumn()
+    role:Role;
+}
