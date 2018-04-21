53e5830f18e33a56481117cc09375c247d9b4152
Josh O'Brian <joshuaeobrian@gmail.com>
2018-04-21 17:59:50 -0400

    Pushing changes from late night session

--- src/routes.ts
+++ src/routes.ts
@@ -1,33 +1,6 @@
-import {UserController} from "./controller/UserController";
 import {USER_ROUTES} from "./route/UserRoutes";
 import {ROLE_ROUTE} from "./route/RoleRoutes";
 
 export const Routes = []
     .concat(USER_ROUTES)
     .concat(ROLE_ROUTE);
-
-
-
-// export const Routes =[
-//     {
-//         method: "get",
-//         route: "/users",
-//         controller: UserController,
-//         action: "all"
-//     }, {
-//     method: "get",
-//     route: "/users/:id",
-//     controller: UserController,
-//     action: "one"
-// }, {
-//     method: "post",
-//     route: "/users",
-//     controller: UserController,
-//     action: "save"
-// }, {
-//     method: "delete",
-//     route: "/users",
-//     controller: UserController,
-//     action: "disableUser"
-// }
-// ];
